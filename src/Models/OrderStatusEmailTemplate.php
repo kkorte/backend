11e727a13d1073907805befa5cbbce061df1418a
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2017-04-16 08:54:08 +0200

    clean up code

--- src/Models/OrderStatusEmailTemplate.php
+++ src/Models/OrderStatusEmailTemplate.php
@@ -15,10 +15,4 @@ class OrderStatusEmailTemplate extends BaseModel
 
     // Add the 'avatar' attachment to the fillable array so that it's mass-assignable on this model.
     protected $fillable = ['title', 'subject', 'content', 'shop_id', 'modified_by_user_id'];
-
-    public function __construct(array $attributes = array())
-    {
-        $this->table = config()->get('hideyo.db_prefix').$this->table;  
-        parent::__construct($attributes);
-    }
 }
